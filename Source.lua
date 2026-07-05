d6f1114cdc66978a2badc97b42ed8c5677b4c89b
Luatic <krokosiv851@gmail.com>
2026-07-05 15:08:41 +0930

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -919,7 +919,7 @@ function Exodus:Init(config)
                         Parent = Selector,
                         BackgroundTransparency = 1,
                         AnchorPoint = Vector2.new(1, 0.5),
-                        Position = UDim2.new(1, -6, 0.5, 0),
+                        Position = UDim2.new(1, -8, 0.5, 0),
                         Size = UDim2.fromOffset(14, 14),
                         Image = "rbxassetid://131833120209646",
                         ImageColor3 = Theme.SubText,
